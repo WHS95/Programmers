9dbe7a8c27c5e6922a7750101262d22e8561e54a
Woohyeok_95 <96816022+WHS95@users.noreply.github.com>
2023-05-25 23:45:39 +0900

    [unrated] Title: 숫자 변환하기, Time: 0.93 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+     public int solution(int x, int y, int n) {
+
+    int cnt = 0;
+    HashSet<Integer> curr = new HashSet<>(), next = null;
+    curr.add(x);
+
+    while (!curr.isEmpty()) {
+        if (curr.contains(y))
+            return cnt;
+        next = new HashSet<>();
+        for (int val : curr) {
+            int pVal = val + n;
+            int dVal = val * 2;
+            int tVal = val * 3;
+            if (pVal <= y) next.add(pVal);
+            if (dVal <= y) next.add(dVal);
+            if (tVal <= y) next.add(tVal);
+        }
+        curr = next;
+        cnt++;
+    }
+    return -1;
+}
+}
